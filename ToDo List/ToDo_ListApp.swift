8ab14cf6e070cd8194540796e154d2859e60f999
Kevin Quinália <kevin.quinalia@ifood.com.br>
2022-05-18 04:02:30 -0300

    Initial Commit

---
+++ ToDo List/ToDo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_ListApp.swift
+//  ToDo List
+//
+//  Created by kevin.quinalia on 18/05/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
